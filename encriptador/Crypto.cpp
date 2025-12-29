090f9fd8c1fa0b8adcd4d076df5dc8f352da8ab8
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-29 12:29:10 +0100

    [Implementing] Crypto.cpp with Encrypt / Decrypt function

--- encriptador/Crypto.cpp
+++ encriptador/Crypto.cpp
@@ -1,9 +1,43 @@
 #include "Crypto.h"
+#include <cctype>
 
-std::string Crypto::Encrypt(const std::string& text, int shift, int& checksum) {
-	return std::string();
-}
+namespace Crypto {
+	static char InvertCase(char c) {
+		if(std::islower(c))
+			return std::toupper(c);
+		if(std::isupper(c))
+			return std::tolower(c);
+		return c;
+	}
 
-std::string Crypto::Decrypt(const std::string& text, int shift, int& checksum) {
-	return std::string();
+	std::string Encrypt(const std::string& text, int shift, int& checksum) {
+		std::string result;
+		checksum = 0;
+
+		for(char c : text) {
+			char inverted = InvertCase(c);
+			char encrypted = inverted + shift;
+
+			checksum += static_cast<int>(encrypted);
+			result += encrypted;
+		}
+
+		return result;
+	}
+
+	std::string Decrypt(const std::string& text, int shift, int& checksum) {
+		std::string result;
+		checksum = 0;
+
+		for(char c : text) {
+			checksum += static_cast<int>(c);
+
+			char decrypted = c - shift;
+			char original = InvertCase(decrypted);
+
+			result += original;
+		}
+
+		return result;
+	}
 }
